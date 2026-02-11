88fbdf53bb19e5e03a2db3e6acad1b695c6c2ce5
Mae Maricar R. Yap <mryap2@up.edu.ph>
2026-02-11 14:44:33 +0800

    update executor.c

--- lab1/src/executor.c
+++ lab1/src/executor.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 
 #include "executor.h"
 #include "jobs.h"
 
-void execute_command(Command *cmd) {
-    // TODO:
-    // - handle built-in commands (exit, cd, pwd)
-    // - fork and exec external commands
-    // - handle I/O redirection
-    // - handle background execution
+void execute_command(Command *cmd)
+{
+    if (cmd->command == NULL)
+        return;
+
+    // -------------------------
+    // BUILT-IN COMMANDS
+    // -------------------------
+
+    // exit
+    if (strcmp(cmd->command, "exit") == 0) {
+        exit(0);
+    }
+
+    // cd
+    if (strcmp(cmd->command, "cd") == 0) {
+        if (cmd->args[1] == NULL) {
+            fprintf(stderr, "cd: missing argument\n");
+        } else {
+            if (chdir(cmd->args[1]) != 0)
+                perror("cd");
+        }
+        return;
+    }
+
+    // pwd
+    if (strcmp(cmd->command, "pwd") == 0) {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+            printf("%s\n", cwd);
+        else
+            perror("pwd");
+        return;
+    }
+
+    // -------------------------
+    // EXTERNAL COMMANDS
+    // -------------------------
 
     pid_t pid = fork();
 
@@ -20,15 +54,16 @@ void execute_command(Command *cmd) {
     }
 
     if (pid == 0) {
-        // child
+        // CHILD PROCESS
         execvp(cmd->command, cmd->args);
         perror("execvp");
-        _exit(127);
+        exit(127);
     } else {
-        // parent
+        // PARENT PROCESS
+
         if (cmd->background) {
             add_background_job(pid);
-            printf("[bg] started pid %d\n", pid);
+            printf("[bg] started PID %d\n", pid);
         } else {
             waitpid(pid, NULL, 0);
         }
